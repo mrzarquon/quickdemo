41572f9d8c89acbc773f761ae1a7e9f375f09f80
Chris Barker <cbarker@puppetlabs.com>
2014-05-18 11:05:16 -0700

    adds example first bootscript
    this uses the simplified agent installer to do all the heavy lifting for
    us and is dependent on autosigning being enabled on the master for fancy
    first run stuff to work

---
+++ files/el6_init.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PUPPET_MASTER="Insert Puppet Master's name here"
+PUPPET_ROLE="webserver"
+
+
+mkdir -p /etc/puppetlabs/facter/facts.d
+
+echo "role=${PUPPET_ROLE}" > /etc/puppetlabs/facter/facts.d/role.txt
+
+curl -sk https://${PUPPET_MASTER}/packages/current/install.bash | /bin/bash
+
+
